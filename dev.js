c75ca965d5d3ac9dca02ae4b80765964cfaca30a
Anish D <anish-11366@anish-11366.csez.zohocorpin.com>
2024-06-03 19:18:04 +0530

    Dev.To Front end challenge May 2024

---
+++ dev.js
@@ -0,0 +1,5 @@
+const title = 'Best Beaches in the World';
+window.onload = function(){
+    document.getElementById('title').innerHTML = title;
+    document.getElementById('header').innerHTML = title;
+};
